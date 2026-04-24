f4e9df71c34c05e543a4423c40aacb4492d5ad3d
ravikiran-personal <ravikiran96@gmail.com>
2026-04-24 21:49:10 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,18 @@
-const CACHE_NAME = "recomp-tracker-v1";
-const URLS_TO_CACHE = ["/"];
+const CACHE_NAME = "recomp-tracker-v2";
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+    })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request).catch(() => {
-          return caches.match("/");
-        })
-      );
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
